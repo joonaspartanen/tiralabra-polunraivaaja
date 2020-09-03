22c889f82df6d1ef7748ee6266ecef3a71832f4e
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-03 16:46:58 +0300

    Alusta Gradle-projekti

---
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/Main.java
@@ -0,0 +1,17 @@
+
+package tiralabra.polunraivaaja;
+
+/**
+ *
+ * @author Joonas Partanen / joonas.partanen@helsinki.fi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
