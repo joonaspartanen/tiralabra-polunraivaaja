3e1ff12b667d524c8b1678b31287ce7cbffdd7d0
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-06 18:40:15 +0300

    Alustettu main-luokka

--- polunraivaaja/src/main/java/tiralabra/polunraivaaja/Main.java
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/Main.java
@@ -1,12 +1,16 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tiralabra.polunraivaaja;
 
 /**
  *
- * @author Joonas Partanen / joonas.partanen@helsinki.fi
+ * @author Joonas Partanen <joonas.partanen@helsinki.fi>
  */
 public class Main {
-
+    
     /**
      * @param args the command line arguments
      */
