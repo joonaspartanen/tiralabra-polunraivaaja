022a26477ed798e83e8381bb0a851ce71e8a9803
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-06 18:40:56 +0300

    Testattu että junit toimii

---
+++ polunraivaaja/src/test/java/tiralabra/polunraivaaja/algoritmit/LeveyshakuTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralabra.polunraivaaja.algoritmit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author Joonas Partanen <joonas.partanen@helsinki.fi>
+ */
+public class LeveyshakuTest {
+
+    public LeveyshakuTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello() {
+        assertEquals(1, 1);
+    }
+}
