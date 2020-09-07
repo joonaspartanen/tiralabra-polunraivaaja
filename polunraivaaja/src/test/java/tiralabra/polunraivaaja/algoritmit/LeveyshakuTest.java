09bd1326c15d8b1eeac82d11dc4819cf0dd5a84e
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-07 19:05:28 +0300

    Ensimmäiset testit leveyshaulle

--- polunraivaaja/src/test/java/tiralabra/polunraivaaja/algoritmit/LeveyshakuTest.java
+++ polunraivaaja/src/test/java/tiralabra/polunraivaaja/algoritmit/LeveyshakuTest.java
@@ -5,8 +5,18 @@
  */
 package tiralabra.polunraivaaja.algoritmit;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import tiralabra.polunraivaaja.kartta.Kartta;
+import tiralabra.polunraivaaja.apurakenteet.Koordinaatti;
+import tiralabra.polunraivaaja.io.Kartanlukija;
 
 /**
  *
@@ -14,14 +24,44 @@ import org.junit.Test;
  */
 public class LeveyshakuTest {
 
-    public LeveyshakuTest() {
+    private Kartanlukija lukija;
+
+    @Before
+    public void setUp() {
+        lukija = new Kartanlukija("kartat/testikartat");
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
     @Test
-    public void hello() {
-        assertEquals(1, 1);
+    public void loytaaReitinTriviaaliKartta() {
+        Kartta kartta = lukija.lueKarttatiedosto("test_0_5.map");
+        Haku leveyshaku = new Leveyshaku(kartta);
+
+        leveyshaku.etsiReitti(new Koordinaatti(0, 0), new Koordinaatti(4, 4));
+
+        List<Koordinaatti> reitti = leveyshaku.getReitti();
+        assertFalse(reitti.isEmpty());
     }
+
+    @Test
+    public void loytaaLyhimmänReitinTriviaaliKartta() {
+        Kartta kartta = lukija.lueKarttatiedosto("test_0_5.map");
+        Haku leveyshaku = new Leveyshaku(kartta);
+
+        leveyshaku.etsiReitti(new Koordinaatti(0, 0), new Koordinaatti(4, 4));
+
+        List<Koordinaatti> reitti = leveyshaku.getReitti();
+        assertThat(reitti.size(), is(5));
+    }
+
+    @Test
+    public void loytaaLyhimmanReitinHelppoKartta() {
+        Kartta kartta = lukija.lueKarttatiedosto("test_0_10.map");
+        Haku leveyshaku = new Leveyshaku(kartta);
+
+        leveyshaku.etsiReitti(new Koordinaatti(0, 0), new Koordinaatti(9, 9));
+
+        List<Koordinaatti> reitti = leveyshaku.getReitti();
+        assertThat(reitti.size(), is(17));
+    }
+
 }
