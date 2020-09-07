a426d990ab5fce12ba53cf986fef9f3105484627
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-07 19:02:50 +0300

    Luo luokka koordinaateille

---
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/apurakenteet/Koordinaatti.java
@@ -0,0 +1,56 @@
+package tiralabra.polunraivaaja.apurakenteet;
+
+public class Koordinaatti {
+
+    private int rivi;
+    private int sarake;
+
+    public Koordinaatti(int rivi, int sarake) {
+        this.rivi = rivi;
+        this.sarake = sarake;
+    }
+
+    public int getRivi() {
+        return rivi;
+    }
+
+    public int getSarake() {
+        return sarake;
+    }
+
+    @Override
+    public int hashCode() {
+        final int alkuluku = 31;
+        int tulos = 1;
+        tulos = alkuluku * tulos + rivi;
+        tulos = alkuluku * tulos + sarake;
+        return tulos;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Koordinaatti toinen = (Koordinaatti) obj;
+        if (rivi != toinen.rivi) {
+            return false;
+        }
+        if (sarake != toinen.sarake) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Koordinaatti: " + rivi + ", " + sarake;
+    }
+
+}
