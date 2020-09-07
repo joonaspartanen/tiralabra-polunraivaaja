442d346b2a5f6d3e03d79388df95e53ae5441fc8
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-07 19:02:25 +0300

    Toteuta leveyshaku

--- polunraivaaja/src/main/java/tiralabra/polunraivaaja/algoritmit/Leveyshaku.java
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/algoritmit/Leveyshaku.java
@@ -1,9 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tiralabra.polunraivaaja.algoritmit;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+import tiralabra.polunraivaaja.kartta.Kartta;
+import tiralabra.polunraivaaja.apurakenteet.Koordinaatti;
 
 /**
  *
@@ -11,9 +14,113 @@ package tiralabra.polunraivaaja.algoritmit;
  */
 public class Leveyshaku implements Haku {
 
-    @Override
-    public String etsiReitti() {
-        return "Toimiiko?";
+    private Kartta kartta;
+    private int korkeus;
+    private int leveys;
+    private Koordinaatti[][] edeltajat;
+    private boolean[][] vierailtu;
+    private Koordinaatti alku;
+    private Koordinaatti loppu;
+
+    List<Koordinaatti> reitti;
+
+    private int[] rivisuunnat = {-1, 0, 0, 1, -1, -1, 1, 1};
+    private int[] sarakesuunnat = {0, -1, 1, 0, -1, 1, -1, 1};
+
+    public Leveyshaku(Kartta kartta) {
+        this.kartta = kartta;
     }
-    
+
+    @Override
+    public void setKartta(Kartta kartta) {
+        this.kartta = kartta;
+    }
+
+    @Override
+    public boolean etsiReitti(Koordinaatti alku, Koordinaatti loppu) {
+
+        korkeus = kartta.getKorkeus();
+        leveys = kartta.getLeveys();
+
+        if (!reitinPaatVapaat(alku, loppu)) {
+            System.out.println("Alku- tai loppupiste ei kelpaa.");
+            return false;
+        }
+
+        this.alku = alku;
+        this.loppu = loppu;
+
+        Queue<Koordinaatti> jono = new ArrayDeque<>();
+        vierailtu = new boolean[korkeus][leveys];
+        edeltajat = new Koordinaatti[korkeus][leveys];
+
+        jono.add(alku);
+        vieraile(alku.getRivi(), alku.getSarake());
+
+        while (!jono.isEmpty()) {
+
+            Koordinaatti ruutu = jono.remove();
+
+            if (loppuSaavutettu(ruutu.getRivi(), ruutu.getSarake())) {
+                muodostaReitti();
+                return true;
+            }
+
+            for (int i = 0; i < 8; i++) {
+                int rivi = ruutu.getRivi() + rivisuunnat[i];
+                int sarake = ruutu.getSarake() + sarakesuunnat[i];
+
+                if (ruutuKelpaa(rivi, sarake) && !vierailtu[rivi][sarake]) {
+                    vieraile(rivi, sarake);
+                    jono.add(new Koordinaatti(rivi, sarake));
+                    edeltajat[rivi][sarake] = ruutu;
+                }
+            }
+        }
+        System.out.println("Reitti ei mahdollinen.");
+        return false;
+    }
+
+    private boolean reitinPaatVapaat(Koordinaatti... koordinaatit) {
+        for (Koordinaatti koordinaatti : koordinaatit) {
+            if (!ruutuKelpaa(koordinaatti.getRivi(), koordinaatti.getSarake())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean ruutuKelpaa(int rivi, int sarake) {
+        return ruutuKartalla(rivi, sarake) && kartta.ruutuVapaa(rivi, sarake);
+    }
+
+    private boolean ruutuKartalla(int rivi, int sarake) {
+        return rivi >= 0 && rivi < korkeus && sarake >= 0 && sarake < leveys;
+    }
+
+    private void vieraile(int rivi, int sarake) {
+        vierailtu[rivi][sarake] = true;
+    }
+
+    private boolean loppuSaavutettu(int rivi, int sarake) {
+        return rivi == loppu.getRivi() && sarake == loppu.getSarake();
+    }
+
+    private void muodostaReitti() {
+        reitti = new ArrayList<>();
+        reitti.add(loppu);
+
+        Koordinaatti ruutu = edeltajat[loppu.getRivi()][loppu.getSarake()];
+        reitti.add(ruutu);
+
+        while (!(ruutu.getRivi() == alku.getRivi() && ruutu.getSarake() == alku.getSarake())) {
+            ruutu = edeltajat[ruutu.getRivi()][ruutu.getSarake()];
+            reitti.add(ruutu);
+        }
+    }
+
+    @Override
+    public List<Koordinaatti> getReitti() {
+        return reitti;
+    }
 }
