02c44fd3964723b851a1bd0e7870bcb4bf12a98c
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-09 22:29:05 +0300

    Alustavaa JavaDocia lisätty

--- polunraivaaja/src/main/java/tiralabra/polunraivaaja/algoritmit/Haku.java
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/algoritmit/Haku.java
@@ -13,7 +13,21 @@ public interface Haku {
 
     void setKartta(Kartta kartta);
 
+    /**
+     * Etsii jonkin lyhimmistä reiteistä parametreina saatujen alku- ja
+     * loppupisteiden välillä.
+     *
+     * @param alku Haettavan reitin alkupiste.
+     * @param loppu Haettavan reitin loppupiste.
+     * @return True, jos reitti löytyi; false, jos reittiä ei löytynyt tai ei
+     * voitu hakea.
+     */
     boolean etsiReitti(Koordinaatti alku, Koordinaatti loppu);
 
+    /**
+     * Palauttaa haun löytämää reittiä kuvaavan listan Koordinaatteja.
+     *
+     * @return Lista Koordinaatteja.
+     */
     List<Koordinaatti> getReitti();
 }
