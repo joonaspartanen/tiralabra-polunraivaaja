47f38d5175cac15e972d6679530bf9dffd29cd33
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-09 22:33:18 +0300

    Lisätty käyttöliittymään ensimmäiset toiminnot

--- polunraivaaja/src/main/java/tiralabra/polunraivaaja/ui/GUI.java
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/ui/GUI.java
@@ -1,10 +1,13 @@
 package tiralabra.polunraivaaja.ui;
 
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import tiralabra.polunraivaaja.algoritmit.Haku;
@@ -16,6 +19,13 @@ import tiralabra.polunraivaaja.kartta.Kartta;
 
 public class GUI extends Application {
 
+    private Canvas karttapohja;
+    private Koordinaatti alkupiste;
+    private Koordinaatti loppupiste;
+    Label alkupisteLabel;
+    Label loppupisteLabel;
+    HBox hakupalkki;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Polunraivaaja");
@@ -25,13 +35,66 @@ public class GUI extends Application {
         GraafinenKartanpiirtaja piirtaja = new GraafinenKartanpiirtaja(kartta);
 
         Haku haku = new Leveyshaku(kartta);
-        haku.etsiReitti(new Koordinaatti(0, 0), new Koordinaatti(511, 511));
-        piirtaja.piirraKartta(haku.getReitti());
 
-        VBox mainContainer = new VBox();
-        mainContainer.getChildren().add(piirtaja.getKarttaruudukko());
-        primaryStage.setScene(new Scene(mainContainer, 1600, 1000));
+        piirtaja.piirraKartta();
+
+        VBox mainContainer = new VBox(20);
+
+        karttapohja = piirtaja.getKarttapohja();
+        mainContainer.getChildren().add(karttapohja);
+
+        karttapohja.setOnMouseClicked(e -> {
+            if (alkupiste == null) {
+                alkupiste = new Koordinaatti((int) e.getY(), (int) e.getX());
+                piirtaja.piirraPiste(alkupiste);
+            } else if (loppupiste == null) {
+                loppupiste = new Koordinaatti((int) e.getY(), (int) e.getX());
+                piirtaja.piirraPiste(loppupiste);
+            } else {
+                alkupiste = null;
+                loppupiste = null;
+                piirtaja.piirraKartta();
+            }
+            karttapohja = piirtaja.getKarttapohja();
+            paivitaHakupalkki();
+        });
+
+        Button piirraReittiNappi = new Button("Piirrä reitti");
+        piirraReittiNappi.setOnAction(e -> {
+            if (alkupiste == null || loppupiste == null) {
+                return;
+            }
+            haku.etsiReitti(alkupiste, loppupiste);
+            List<Koordinaatti> reitti = haku.getReitti();
+            piirtaja.piirraKartta(reitti);
+            karttapohja = piirtaja.getKarttapohja();
+        });
+
+        alkupisteLabel = new Label("Alku: ");
+        alkupisteLabel.setMinWidth(120);
+        loppupisteLabel = new Label("Loppu: ");
+        loppupisteLabel.setMinWidth(120);
+
+        Button pyyhiReittiNappi = new Button("Pyyhi reitti");
+
+        pyyhiReittiNappi.setOnAction(e -> {
+            piirtaja.piirraKartta();
+            karttapohja = piirtaja.getKarttapohja();
+        });
+
+
+        hakupalkki = new HBox(alkupisteLabel, loppupisteLabel, piirraReittiNappi, pyyhiReittiNappi);
+        hakupalkki.setSpacing(20);
+
+        mainContainer.getChildren().add(hakupalkki);
+
+        primaryStage.setScene(new Scene(mainContainer, 1600, 1200));
         primaryStage.show();
+    }
+
+    private void paivitaHakupalkki() {
+        alkupisteLabel.setText(alkupiste == null ? "Alku: " : "Alku: " + alkupiste.toString());
+        loppupisteLabel.setText(loppupiste == null ? "Loppu: " : "Loppu: " + loppupiste.toString());
     }
 
     public static void main(String[] args) {
