f4c6363207fcedfc2c52631eac07d9fa96cd88ef
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-13 22:14:38 +0300

    Lisätty Haku-rajapinta takaisin

--- polunraivaaja/src/main/java/tiralabra/polunraivaaja/algoritmit/Haku.java
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/algoritmit/Haku.java
@@ -1,9 +1,8 @@
 package tiralabra.polunraivaaja.algoritmit;
 
 import java.util.List;
-
-import tiralabra.polunraivaaja.kartta.Kartta;
 import tiralabra.polunraivaaja.apurakenteet.Koordinaatti;
+import tiralabra.polunraivaaja.kartta.Kartta;
 
 /**
  *
@@ -11,23 +10,16 @@ import tiralabra.polunraivaaja.apurakenteet.Koordinaatti;
  */
 public interface Haku {
 
-    void setKartta(Kartta kartta);
 
-    /**
-     * Etsii jonkin lyhimmistä reiteistä parametreina saatujen alku- ja
-     * loppupisteiden välillä.
-     *
-     * @param alku Haettavan reitin alkupiste.
-     * @param loppu Haettavan reitin loppupiste.
-     * @return True, jos reitti löytyi; false, jos reittiä ei löytynyt tai ei
-     * voitu hakea.
-     */
     boolean etsiReitti(Koordinaatti alku, Koordinaatti loppu);
-
+  
     /**
      * Palauttaa haun löytämää reittiä kuvaavan listan Koordinaatteja.
      *
      * @return Lista Koordinaatteja.
      */
     List<Koordinaatti> getReitti();
+
+    void setKartta(Kartta kartta);
+    
 }
