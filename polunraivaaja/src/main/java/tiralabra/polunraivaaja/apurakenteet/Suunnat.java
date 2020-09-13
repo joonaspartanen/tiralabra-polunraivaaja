56c54659629be82e3547fec4607dde97e1503d44
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-13 22:17:41 +0300

    Viety suunnat omaan luokkaansa

---
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/apurakenteet/Suunnat.java
@@ -0,0 +1,8 @@
+package tiralabra.polunraivaaja.apurakenteet;
+
+public class Suunnat {
+
+  public static final int[] riviSiirtymat = { -1, 0, 0, 1 };
+  public static final int[] sarakeSiirtymat = { 0, -1, 1, 0 };
+
+}
