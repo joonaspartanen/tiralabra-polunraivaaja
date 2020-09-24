d47d961cb384cc8e6269108b2f0495bef9a33b9f
Joonas Partanen <partanen.joonas@gmail.com>
2020-09-24 18:13:01 +0300

    Poistettu konsolikartanpiirtäjä turhana

--- polunraivaaja/src/main/java/tiralabra/polunraivaaja/kartta/Kartanpiirtaja.java
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/kartta/Kartanpiirtaja.java
@@ -2,45 +2,131 @@ package tiralabra.polunraivaaja.kartta;
 
 import java.util.List;
 
-import tiralabra.polunraivaaja.apurakenteet.Koordinaatti;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import tiralabra.polunraivaaja.apurakenteet.Ruutu;
 
 /**
- * Abstrakti luokka, jonka toteuttavat luokat piirtävät karttoja esim. konsoliin
- * tai graafiseen käyttöliittymään.
+ * Piirtää kartan graafiseen käyttöliittymään.
  *
  * @author Joonas Partanen <joonas.partanen@helsinki.fi>
+ *
  */
-public abstract class Kartanpiirtaja {
+public class Kartanpiirtaja {
 
-    protected Kartta kartta;
-    protected int korkeus;
-    protected int leveys;
+    private Kartta kartta;
+    private int korkeus;
+    private int leveys;
+    private Canvas karttapohja;
+    private GraphicsContext gc;
+    private final double KARTTAPOHJAN_LEVEYS = 512;
+    private final double KARTTAPOHJAN_KORKEUS = 512;
 
+    private double ruudunLeveys;
+    private double ruudunKorkeus;
+
+    /**
+     * @param kartta Piirrettävä kartta.
+     */
     public Kartanpiirtaja(Kartta kartta) {
         this.kartta = kartta;
         this.korkeus = kartta.getKorkeus();
         this.leveys = kartta.getLeveys();
+        karttapohja = new Canvas(KARTTAPOHJAN_LEVEYS, KARTTAPOHJAN_KORKEUS);
+        gc = karttapohja.getGraphicsContext2D();
+        alustaKarttapohja();
     }
 
     /**
      * Piirtää pelkän pohjakartan.
      */
-    public abstract void piirraKartta();
+    public void piirraKartta() {
+        for (int i = 0; i < korkeus; i++) {
+            for (int j = 0; j < leveys; j++) {
+                piirraRuutu(i, j, false, false);
+            }
+        }
+    }
 
     /**
      * Piirtää pohjakartan ja siihen parametrina saadun reitin.
      *
      * @param reitti Pohjakartan päälle piirrettävä reitti.
      */
-    public abstract void piirraKartta(List<Koordinaatti> reitti);
+    public void piirraKartta(List<Ruutu> reitti) {
+        for (int i = 0; i < korkeus; i++) {
+            for (int j = 0; j < leveys; j++) {
+                boolean kuuluuReittiin = reitti.contains(new Ruutu(i, j));
+                piirraRuutu(i, j, kuuluuReittiin, false);
+            }
+        }
+    }
 
     /**
-     * Piirtää pohjakartan ja siihen parametrina saadun reitin sekä vieraillut
-     * solmut.
      *
-     * @param reitti    Pohjakartan päälle piirrettävä reitti.
-     * @param vierailtu boolean-taulukko, joka kertoo missä solmuissa on vierailtu.
+     * Piirtää pohjakartan ja siihen parametrina saadun reitin sekä vieraillut
+     * ruudut.
+     *
      *
      */
-    public abstract void piirraKartta(List<Koordinaatti> reitti, boolean[][] vierailtu);
+    public void piirraKartta(List<Ruutu> reitti, boolean[][] vierailtu) {
+        for (int i = 0; i < korkeus; i++) {
+            for (int j = 0; j < leveys; j++) {
+                boolean kuuluuReittiin = reitti.contains(new Ruutu(i, j));
+                boolean ruudussaVierailtu = vierailtu[i][j];
+                piirraRuutu(i, j, kuuluuReittiin, ruudussaVierailtu);
+            }
+        }
+    }
+
+    private void piirraRuutu(int rivi, int sarake, boolean kuuluuReittiin, boolean ruudussaVierailtu) {
+        if (kuuluuReittiin) {
+            gc.setFill(Color.CRIMSON);
+        } else if (ruudussaVierailtu) {
+            gc.setFill(Color.GREENYELLOW);
+        } else {
+            gc.setFill(kartta.ruutuVapaa(rivi, sarake) ? Color.WHITESMOKE : Color.BLACK);
+        }
+        gc.fillRect(sarake * ruudunLeveys, rivi * ruudunKorkeus, ruudunLeveys, ruudunKorkeus);
+    }
+
+    /**
+     *
+     * @return Canvas-olio, joka muodostaa kartan.
+     */
+    public Canvas getKarttapohja() {
+        return karttapohja;
+    }
+
+    /**
+     * Käytetään piirtämään karttaan reitin alku- ja loppupisteet.
+     *
+     * TODO: Poista taikanumerot.
+     */
+    public void piirraPiste(Ruutu sijainti) {
+        gc.setFill(Color.CRIMSON);
+        gc.fillOval(sijainti.getSarake() * ruudunLeveys - 3, sijainti.getRivi() * ruudunKorkeus - 3, 7, 7);
+    }
+
+    public double getRuudunLeveys() {
+        return ruudunLeveys;
+    }
+
+    public double getRuudunKorkeus() {
+        return ruudunKorkeus;
+    }
+
+    public void setKartta(Kartta kartta) {
+        this.kartta = kartta;
+        this.korkeus = kartta.getKorkeus();
+        this.leveys = kartta.getLeveys();
+        alustaKarttapohja();
+    }
+
+    private void alustaKarttapohja() {
+        this.ruudunLeveys = KARTTAPOHJAN_LEVEYS / leveys;
+        this.ruudunKorkeus = KARTTAPOHJAN_KORKEUS / korkeus;
+        gc.clearRect(0, 0, leveys, korkeus);
+    }
 }
