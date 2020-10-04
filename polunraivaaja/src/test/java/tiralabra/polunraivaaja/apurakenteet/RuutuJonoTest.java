5285caabe2374cd6950e186ffcf45e0f80d6c52e
Joonas Partanen <partanen.joonas@gmail.com>
2020-10-04 18:09:55 +0300

    Testit omalle jono-implementaatiolle

---
+++ polunraivaaja/src/test/java/tiralabra/polunraivaaja/apurakenteet/RuutuJonoTest.java
@@ -0,0 +1,52 @@
+package tiralabra.polunraivaaja.apurakenteet;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class RuutuJonoTest {
+
+  private RuutuJono jono;
+
+  @Before
+  public void luoTestiJono() {
+    this.jono = new RuutuJono();
+    this.jono.lisaaJonoon(new Ruutu(0, 0));
+    this.jono.lisaaJonoon(new Ruutu(1, 1));
+    this.jono.lisaaJonoon(new Ruutu(2, 2));
+  }
+
+  @Test
+  public void ruutujaVoiLisataJonoon() {
+
+    assertThat(jono.haePituus(), is(3));
+  }
+
+  @Test
+  public void otaJonostaPalauttaaOikeanRuudun() {
+    Ruutu ruutu = jono.otaJonosta();
+    assertThat(ruutu.x, is(0));
+    assertThat(ruutu.y, is(0));
+
+    ruutu = jono.otaJonosta();
+    assertThat(ruutu.x, is(1));
+    assertThat(ruutu.y, is(1));
+
+    ruutu = jono.otaJonosta();
+    assertThat(ruutu.x, is(2));
+    assertThat(ruutu.y, is(2));
+  }
+
+  @Test
+  public void otaJonostaPalauttaaNullJosJonoTyhja() {
+    for (int i = 0; i < 3; i++) {
+      jono.otaJonosta();
+    }
+    Ruutu ruutu = jono.otaJonosta();
+    assertThat(ruutu, is(nullValue()));
+  }
+}
