c92c9f8526b4e71ed312bda3566cf7c5bc859f72
Joonas Partanen <partanen.joonas@gmail.com>
2020-10-13 21:51:14 +0300

    Viety toistuva koodi yläluokkaan

--- polunraivaaja/src/main/java/tiralabra/polunraivaaja/suorituskykytestit/tietorakenteet/Vertailutulos.java
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/suorituskykytestit/tietorakenteet/Vertailutulos.java
@@ -1,30 +1,28 @@
 package tiralabra.polunraivaaja.suorituskykytestit.tietorakenteet;
 
+import tiralabra.polunraivaaja.suorituskykytestit.Suorituskykytulos;
+
 /**
  * Omia ja Javan valmiita tietorakenteita testaavien suorituskykytestien
  * tulokset kääritään Vertailutulos-luokkaan.
  *
  * @author Joonas Partanen <joonas.partanen@helsinki.fi>
  */
-public class Vertailutulos {
+public class Vertailutulos extends Suorituskykytulos {
 
     private long omanRakenteenAika;
     private long javaRakenteenAika;
-    private int iteraatioita;
 
     public Vertailutulos(long omanRakenteenAika, long javaRakenteenAika, int iteraatioita) {
+        super(iteraatioita);
         this.omanRakenteenAika = omanRakenteenAika;
         this.javaRakenteenAika = javaRakenteenAika;
         this.iteraatioita = iteraatioita;
     }
 
-    private long aikaMillisekunteina(long aika) {
-        return aika / 1000000;
-    }
-
     @Override
     public String toString() {
-        return "SUORITUSKYKYVERTAILU: \n" + "Iteraatioita: " + iteraatioita + "\n" + "Oman rakenteen aika: "
+        return "\n SUORITUSKYKYVERTAILU: \n" + "Iteraatioita: " + iteraatioita + "\n" + "Oman rakenteen aika: "
                 + aikaMillisekunteina(omanRakenteenAika) + " ms \n" + "Javan valmiin tietorakenteen aika "
                 + aikaMillisekunteina(javaRakenteenAika) + " ms";
     }
