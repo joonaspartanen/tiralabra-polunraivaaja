1473c565c9258f70c8b068cf048a2b8f26e57f92
Joonas Partanen <partanen.joonas@gmail.com>
2020-10-17 21:02:17 +0300

    Lisätty luokka kuvaamaan skenaariota

---
+++ polunraivaaja/src/main/java/tiralabra/polunraivaaja/suorituskykytestit/algoritmit/Skenaario.java
@@ -0,0 +1,54 @@
+package tiralabra.polunraivaaja.suorituskykytestit.algoritmit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tiralabra.polunraivaaja.kartta.Kartta;
+
+/**
+ * Reitinhakualgoritmien suorituskykytestit käyttävät aineistonaan Skenaarioita,
+ * joihin liittyy tietty kartta ja joukko reittikuvauksia, joille pyritään
+ * etsimään lyhin reitti.
+ *
+ * @author Joonas Partanen <joonas.partanen@helsinki.fi>
+ */
+public class Skenaario {
+
+    /**
+     * Haun kohteena oleva kartta.
+     */
+    private final Kartta kartta;
+
+    /**
+     * Lista reittien alku- ja loppupisteitä, joille haetaan skenaarion aikana
+     * lyhimmät reitit.
+     */
+    private final List<Reittikuvaus> reittikuvaukset;
+
+    /**
+     * Konstruktori.
+     *
+     * @param kartta Haun kohteena oleva kartta.
+     */
+    public Skenaario(Kartta kartta) {
+        this.kartta = kartta;
+        this.reittikuvaukset = new ArrayList<>();
+    }
+
+    public Kartta getKartta() {
+        return kartta;
+    }
+
+    public List<Reittikuvaus> getReittikuvaukset() {
+        return reittikuvaukset;
+    }
+
+    /**
+     * Lisää yksittäisen reittikuvauksen reittikuvausten listaan.
+     *
+     * @param reittikuvaus Lisättävä Reittikuvaus-olio.
+     */
+    public void lisaaReittikuvaus(Reittikuvaus reittikuvaus) {
+        reittikuvaukset.add(reittikuvaus);
+    }
+}
